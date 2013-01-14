8b8750f39e035694362a1bbdad70698d984dbdad
Zachary Elliott <zach@nyu.edu>
2013-01-14 17:58:07 -0500

    Modifying tests-driver and Makefile to handle using make for compilation of asm files

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 CC = gcc
-RM = rm -Rvf
+RM = rm -Rf
 
 CFLAGS = -ggdb -x c -std=c99 -c -Wall -Wextra -Werror -pedantic-errors -Wdouble-promotion -Wformat=2 -Winit-self -Wmissing-include-dirs -Wparentheses -Wswitch-default -Wswitch-enum -Wsync-nand -Wuninitialized -Wunknown-pragmas -Wstrict-overflow=2 -Wfloat-equal -Wundef -Wpointer-arith -Wbad-function-cast -Wcast-qual -Wconversion -Wlogical-op -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wmissing-format-attribute -Wpacked -Wpacked-bitfield-compat -Wdouble-promotion -Wformat=2 -Winit-self -Wmissing-include-dirs -Wparentheses -Wswitch-default -Wswitch-enum -Wsync-nand -Wuninitialized -Wunknown-pragmas -Wstrict-overflow=2 -Wfloat-equal -Wundef -Wpointer-arith -Wbad-function-cast -Wcast-qual -Wconversion -Wlogical-op -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wmissing-format-attribute -Wpacked -Wpacked-bitfield-compat -Wredundant-decls -Wnested-externs -Winline -Woverlength-strings -Wcomments -Wundef -Wunused-macros -Wendif-labels
 LDFLAGS =
@@ -10,13 +10,23 @@ INCROOT = src/c/include
 TSTROOT = test/c
 
 OBJROOT = build
+BINROOT = bin
 
 VPATH=$(SRCROOT):$(INCROOT):$(TSTROOT):$(OBJROOT)
 
 
 .PHONY: all
 
-all: runtime
+all: stst
+
+.PHONY: clean
+
+clean:
+	$(RM) $(OBJROOT)/*.s
+	$(RM) $(OBJROOT)/*.o
+	$(RM) $(OBJROOT)/runtime
+	$(RM) $(BINROOT)/*
+
 
 ctest.o: ctest.c ctest.h
 	$(CC) $(CFLAGS) -I$(INCROOT) -o $(OBJROOT)/$(@) $(<)
@@ -25,16 +35,22 @@ runtime.o: runtime.c runtime.h
 	$(CC) $(CFLAGS) -I$(INCROOT) -o $(OBJROOT)/$(@) $(<)
 
 runtime: runtime.o ctest.o
-	$(CC) -o $(OBJROOT)/$(@) $(OBJROOT)/runtime.o $(OBJROOT)/ctest.o
+	$(CC) -o $(BINROOT)/$(@) $(OBJROOT)/runtime.o $(OBJROOT)/ctest.o
 
+stst.o: stst.s
+	$(CC) -c -o $(OBJROOT)/$(@) $(<)
 
-.PHONY: clean
+stst: stst.o runtime.o
+	$(CC) -o $(BINROOT)/$(@) $(OBJROOT)/runtime.o $(OBJROOT)/stst.o
 
-clean:
-	$(RM) $(OBJROOT)/*.o
-	$(RM) $(OBJROOT)/runtime
+.PHONY: test_clean
 
+test_clean:
+	$(RM) $(OBJROOT)/stst.s
+	$(RM) $(OBJROOT)/stst.o
+	$(RM) $(BINROOT)/*
+	$(RM) stst.out
 
-.PHONY: rebuild
-
-rebuild: clean all
+.SILENT: stst
+.SILENT: stst.o
+.SILENT: test_clean
--- test/scheme/tests-driver.scm
+++ test/scheme/tests-driver.scm
@@ -1,25 +1,30 @@
 
 (define all-tests '())
 
+(define (compile-program x)
+  (emit-program x))
+
 (define-syntax add-tests-with-string-output
   (syntax-rules (=>)
     [(_ test-name [expr => output-string] ...)
      (set! all-tests
-        (cons 
+        (cons
            '(test-name [expr string  output-string] ...)
             all-tests))]))
 
 (define (run-compile expr)
-  (let ([p (open-output-file "stst.s" 'replace)])
+  (let ([p (open-output-file "build/stst.s" 'replace)])
     (compile-program expr p)
     (close-output-port p)))
 
 (define (build)
-  (unless (zero? (system "gcc -o stst startup.c stst.s"))
+  (unless (zero? (system "make all"))
     (error 'make "could not build target")))
+;;  (unless (zero? (system "gcc -o stst startup.c stst.s"))
+;;    (error 'make "could not build target")))
 
 (define (execute)
-  (unless (zero? (system "./stst > stst.out"))
+  (unless (zero? (system "./bin/stst > stst.out"))
     (error 'make "produced program exited abnormally")))
 
 
@@ -56,13 +61,13 @@
      [(string) (test-with-string-output test-id expr out)]
      [else (error 'test "invalid test type ~s" type)])
     (printf " ok\n")))
- 
+
 (define (test-all)
   (let f ([i 0] [ls (reverse all-tests)])
     (if (null? ls)
         (printf "passed all ~s tests\n" i)
         (let ([x (car ls)] [ls (cdr ls)])
-          (let* ([test-name (car x)] 
+          (let* ([test-name (car x)]
                  [tests (cdr x)]
                  [n (length tests)])
             (printf "Performing ~a tests ...\n" test-name)
@@ -74,14 +79,14 @@
                  (g (add1 i) (cdr tests))])))))))
 
 
-(define input-filter 
+(define input-filter
   (make-parameter (lambda (x) x)
     (lambda (x)
       (unless (procedure? x)
         (error 'input-filter "not a procedure ~s" x))
       x)))
 
-(define runtime-file 
+(define runtime-file
   (make-parameter
     "runtime.c"
     (lambda (fname)
@@ -93,21 +98,21 @@
   (make-parameter
     (current-output-port)
     (lambda (p)
-       (unless (output-port? p) 
+       (unless (output-port? p)
          (error 'compile-port "not an output port ~s" p))
        p)))
 
 (define show-compiler-output (make-parameter #f))
 
 (define (run-compile expr)
-  (let ([p (open-output-file "stst.s" 'replace)])
+  (let ([p (open-output-file "build/stst.s" 'replace)])
     (parameterize ([compile-port p])
        (compile-program expr))
     (close-output-port p)))
 
 
 (define (execute)
-  (unless (fxzero? (system "./stst > stst.out"))
+  (unless (fxzero? (system "./bin/stst > stst.out"))
     (error 'execute "produced program exited abnormally")))
 
 (define (get-string)
@@ -132,4 +137,3 @@
 (define (emit . args)
   (apply fprintf (compile-port) args)
   (newline (compile-port)))
-
